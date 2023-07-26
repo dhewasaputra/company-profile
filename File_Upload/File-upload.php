3037ced7121009c5041eb638c8ecebfba0b58dfe
Dhewasaputra <dhewasaputra8@gmail.com>
2023-07-26 10:27:38 +0700

    update 26/07/2023 page about us author : dhewa

---
+++ File_Upload/File-upload.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <script src="https://kit.fontawesome.com/aed8e6dbd9.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="icon" type="image/x-icon" href="/image/Debito.png">
+
+    <title> Debito Korporindo Utama </title>
+    <script> document.addEventListener("DOMContentLoaded", function(){
+      var myModal = new bootstrap.Modal(document.getElementById('myModal'))
+      myModal.show();
+  });
+  </script>
+
+  </head>
+  <body>
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light " style="height: 150px;" >
+      <div class="container px-5">
+         <img src="/image/navbar.png" alt="logo" style="width: 50%;"> 
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent" style="border-top:1px solid transparent; box-shadow: inset 0 1x 0 rgba ( 255,255,255,0.1)">
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link  active nav-li" aria-current="page" href="index.html"> <i class="fa-solid fa-house"></i> Home</a></li>
+                <li class="nav-item"><a class="nav-link " href="about.html"> <i class="fa-solid fa-people-group"></i> About</a></li>
+                  <li class="nav-item"><a class="nav-link" href="contact .html"> <i class="fa-solid fa-address-book"></i> Contact</a></li>
+                  <li class="nav-item"><a class="nav-link  " href="https://www.jobstreet.co.id/id/job/4350793"> <i class="fa-solid fa-briefcase"></i> Career </a></li>
+                  <li class="nav-item"><a class="nav-link "  href="activity.html"> <i class="fa-solid fa-image"></i> Activity</a></li>
+                  <li class="nav-item"> <a class="nav-link " href="news.html"> <i class="fa-solid fa-newspaper"></i>  News </a></li>
+              </ul>
+          </div>
+      </div>
+  </nav>
+ 
+ <!-- Footer -->
+ <footer class="text-center text-lg-start bg-dark text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-google"> PT Debito Korporindo Utama</i> 
+      </a>
+      <a href="https://instagram.com/debitroops?igshid=Yzg5MTU1MDY=" class="me-4 link-secondary">
+        <i class="fab fa-instagram"> Debitroops</i>
+      </a>
+      <a href="https://www.linkedin.com/company/pt-debito-korporindo-utama/" class="me-4 link-secondary">
+        <i class="fab fa-linkedin"> PT Debito Korporindo Utama</i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            PT Debito Korporindo Utama
+          </h6>
+          <p>
+           PT Debito Korporindo Utama 
+           Ruko Saladin Square Blok B3 
+           Jl. Margonda Raya No.37-39 , Depok , Kec . Pancoran Mas , 
+           Kota Depok, Jawa Barat 16431
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+    
+          </h6>
+           
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3 text-secondary"></i> Jawa Barat , Depok , 16431</p>
+          <p>
+            <i class="fas fa-envelope me-3 text-secondary"></i>
+            info@debitokorp.com
+          </p>
+          <p><i class="fas fa-phone me-3 text-secondary"></i> (021) 5064-5087 </p>
+          <p><i class="fa-brands fa-whatsapp"></i> 081213540606
+          </p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="https://ptdebitokorp.netlify.app">Debito Korporindo Utama </a>
+  </div>
+  <!-- Copyright -->
+</footer>
+      </body>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+   
+</html>
